193a47fc66230d53df514bd98ba9964da0a4243a
Sushant Ray <goodboyneon03@gmail.com>
2026-05-09 08:59:58 +0545

    fix: add redirect link to EXPLORE button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -30,9 +30,12 @@ export default function Navbar() {
           >
             <Heart className="w-6 h-6" />
           </Link>
-          <button className="hidden sm:block bg-black text-white border-2 border-black px-6 py-2 font-sans font-bold uppercase tracking-widest text-xs hover:bg-purple-600 hover:translate-x-1 hover:-translate-y-1 active:translate-x-0 active:translate-y-0 transition-all shadow-[4px_4px_0px_0px_rgba(147,51,234,1)]">
+          <Link
+            href={"/library"}
+            className="hidden sm:block bg-black text-white border-2 border-black px-6 py-2 font-sans font-bold uppercase tracking-widest text-xs hover:bg-purple-600 hover:translate-x-1 hover:-translate-y-1 active:translate-x-0 active:translate-y-0 transition-all shadow-[4px_4px_0px_0px_rgba(147,51,234,1)]"
+          >
             Explore
-          </button>
+          </Link>
         </div>
       </div>
     </nav>
